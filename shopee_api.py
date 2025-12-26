28d7d4e4213571569aefa31bbce5d2b8b61199b8
BeDuy <duylulu100901@gmail.com>
2025-12-27 00:25:49 +0700

    fix missing parse_product

--- shopee_api.py
+++ shopee_api.py
@@ -1,5 +1,4 @@
 import requests
-import random
 import re
 
 HEADERS = {
@@ -14,9 +13,6 @@ HEADERS = {
 }
 
 def extract_ids(link: str):
-    """
-    https://shopee.vn/...-i.123456.987654
-    """
     match = re.search(r'i\.(\d+)\.(\d+)', link)
     if not match:
         raise ValueError("Cannot extract itemid/shopid")
@@ -24,18 +20,24 @@ def extract_ids(link: str):
 
 def fetch_pdp(itemid: str, shopid: str) -> dict:
     url = "https://shopee.vn/api/v4/pdp/get_pc"
+    params = {"itemid": itemid, "shopid": shopid}
 
-    params = {
-        "itemid": itemid,
-        "shopid": shopid
-    }
-
-    r = requests.get(
-        url,
-        headers=HEADERS,
-        params=params,
-        timeout=10
-    )
-
+    r = requests.get(url, headers=HEADERS, params=params, timeout=10)
     r.raise_for_status()
     return r.json()
+
+def parse_product(data: dict) -> dict:
+    item = data["data"]["item"]
+    pb = data["data"].get("price_breakdown")
+
+    return {
+        "item_id": item["item_id"],
+        "shop_id": item["shop_id"],
+        "name": item["title"],
+        "price": item["price"] / 100000,
+        "price_before_discount": item["price_before_discount"] / 100000,
+        "price_after_voucher": (
+            pb.get("price", {}).get("single_value", 0) / 100000
+            if pb else None
+        )
+    }
